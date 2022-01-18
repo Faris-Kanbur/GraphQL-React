080cd10fda0496650ccab74e0b248df0e1ec57a1
faris <faris38kanbur@gmail.com>
2022-01-18 17:25:42 +0100

    add categories

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
-const {mainCards,animals} = require('./db')
+const {mainCards, animals, categories} = require('./db')
 
 const typeDefs = gql`
 type MainCard {
@@ -19,11 +19,20 @@ type Animal {
   onSale:Boolean
     
 }
+
+type Category {
+  id: ID!
+  image: String!
+  category:String!
+  slug:String!
+}
   
 type Query {
     mainCards: [MainCard]
     animals: [Animal!]!
     animal(slug: String):Animal!
+    categories:[Category!]!
+    category(slug:String):Category!
   }
 `;
 
@@ -37,6 +46,14 @@ const resolvers = {
         return animal.slug === args.slug
       })
       return animal
+    },
+
+    categories: () => categories,
+    category: (parent,args,ctx) => {
+      let category = categories.find((category) => {
+        return category.slug === args.slug
+      })
+      return category
     },
   },
 };
