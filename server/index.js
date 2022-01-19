e49dbc3bda7f0e0054739dfa58a58f1e62876c5e
faris <faris38kanbur@gmail.com>
2022-01-19 14:18:29 +0100

    modefy resolver page and typeDefs

--- server/index.js
+++ server/index.js
@@ -1,45 +1,20 @@
 const { ApolloServer } = require('apollo-server');
 const {mainCards, animals, categories} = require('./db')
 const typeDefs = require('./schema')
+const Query = require('./resolvers/Query')
+const Animal = require('./resolvers/Animal')
+const Category = require('./resolvers/Category')
 
-
-const resolvers = {
-  Query: {
-    mainCards: () => mainCards,
-    animals: () => animals,
-    animal: (parent,args,ctx) => {
-      let animal = animals.find((animal) => {
-        return animal.slug === args.slug
-      })
-      return animal
-    },
-
-    categories: () => categories,
-    category: (parent,args,ctx) => {
-      let category = categories.find((category) => {
-        return category.slug === args.slug
-      }) 
-      return category
-    },
-  },
-  Category : {
-    animals: (parent, args, ctx) => {
-      return animals.filter(animal => {
-        return animal.category === parent.id
-      })
-    }
-  },
-  Animal : {
-    category: (parent, args, ctx) => {
-      return categories.find(category => {
-        return category.id === parent.category
-      })
-    }
-  }
-};
-
-
-const server = new ApolloServer({ typeDefs, resolvers });
+ 
+//typeDefs and resolvers
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers:{
+    Query,
+    Animal,
+    Category
+  } 
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
