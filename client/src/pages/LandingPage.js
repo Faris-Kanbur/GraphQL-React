30399edc21a88b1ebe75e912ff752665f6a1d38e
faris <faris38kanbur@gmail.com>
2022-01-20 17:43:08 +0100

    add mutation in react

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
-import { useQuery, gql } from "@apollo/client"
+import { useQuery,useMutation, gql } from "@apollo/client"
 
 const ANIMALS_QUERY = gql`
    { 
@@ -16,9 +16,38 @@ const ANIMALS_QUERY = gql`
     }
 `
 
+const ADD_ANIMAL_MUTATION = gql`
+mutation(
+    $image: String!,
+    $category:String!,
+    $title:String!,
+    $stock:Int!,
+    $price:String!,
+    $description:[String!]!
+    $rating:Floar
+    $slug:String!
+
+){
+    addAnimal(
+    image: $image,
+    category: $category,
+    title: $title,
+    stock:$stock,
+    price: $price,
+    description:$description,
+    rating:$rating,
+    slug:$slug,
+  )
+   { 
+       id 
+    }
+}
+`
+
 function LandingPage() {
 
-    const { loading, error, data } = useQuery(ANIMALS_QUERY)
+    const { loading, error, data } = useQuery(ANIMALS_QUERY);
+    const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION);
 
     if(loading) return <div>Loading...</div>;
 
@@ -29,6 +58,21 @@ function LandingPage() {
             <MainHero />
             <CategoryDisplay />
             <CardDisplay animals={data.animals}/>
+            {/* <button onClick={() => {
+                addAnimal({
+                    variables: {
+                        image:"ostrich",
+                        category:"1",
+                        title:"this is a title",
+                        stock:13,
+                        price: "32,333",
+                        description:["das"],
+                        rating:3.6,
+                        slug:"ostrich",
+
+                    }
+                })
+            }}>Add adn Ostrich</button> */}
         </div>
     )
 }
